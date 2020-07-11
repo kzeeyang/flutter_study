f801b6c2859b605bafc1d62f90547bdfcaae0f34
kzeeyang <2402989406@qq.com>
2020-07-11 10:29:19 +0800

    checkbox

--- lib/demo/checkBox_demo.dart
+++ lib/demo/checkBox_demo.dart
@@ -20,14 +20,31 @@ class _CheckBoxDemoState extends State<CheckBoxDemo> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Checkbox(
+              CheckboxListTile(
                 value: _checkBoxItemA,
                 onChanged: (value) {
                   setState(() {
                     _checkBoxItemA = value;
                   });
                 },
-                activeColor: Colors.black, //设置复选框颜色
+                title: Text('Checkbox Item A'),
+                subtitle: Text('Description'), //子标题
+                secondary: Icon(Icons.bookmark),
+                selected: _checkBoxItemA, //设置激活状态的颜色
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  // Checkbox(
+                  //   value: _checkBoxItemA,
+                  //   onChanged: (value) {
+                  //     setState(() {
+                  //       _checkBoxItemA = value;
+                  //     });
+                  //   },
+                  //   activeColor: Colors.black, //设置复选框颜色
+                  // ),
+                ],
               ),
             ],
           )),
