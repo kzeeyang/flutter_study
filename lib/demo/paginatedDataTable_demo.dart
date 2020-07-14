21929f04d00db3d802f109a806ecc6d3f84dde28
kzeeyang <2402989406@qq.com>
2020-07-14 14:11:05 +0800

    paginatedDataTable demo

---
+++ lib/demo/paginatedDataTable_demo.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import '../model/post.dart';
+
+class PostDataSource extends DataTableSource {
+  final List<Post> _posts = posts;
+  int _selectedCount = 0;
+
+  @override
+  // TODO: implement rowCount
+  int get rowCount => _posts.length;
+
+  @override
+  // TODO: implement isRowCountApproximate
+  bool get isRowCountApproximate => false;
+
+  @override
+  // TODO: implement selectedRowCount
+  int get selectedRowCount => _selectedCount;
+
+  @override
+  DataRow getRow(int index) {
+    final Post post = _posts[index];
+    return DataRow.byIndex(
+      index: index,
+      cells: <DataCell>[
+        DataCell(Text(post.title)),
+        DataCell(Text(post.author)),
+        DataCell(Image.network(post.imageUrl)),
+      ],
+    );
+  }
+
+  void _sort(getField(post), bool ascending) {
+    _posts.sort((a, b) {
+      if (!ascending) {
+        final c = a;
+        a = b;
+        b = c;
+      }
+
+      final aValue = getField(a);
+      final bValue = getField(b);
+
+      return Comparable.compare(aValue, bValue);
+    });
+    notifyListeners();
+  }
+}
+
+class PaginatedDataTableDemo extends StatefulWidget {
+  @override
+  _PaginatedDataTableDemoState createState() => _PaginatedDataTableDemoState();
+}
+
+class _PaginatedDataTableDemoState extends State<PaginatedDataTableDemo> {
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
+
+  final PostDataSource _postsDataSource = PostDataSource();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PaginatedDataTableDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: ListView(
+          children: <Widget>[
+            PaginatedDataTable(
+              header: Text('Posts'),
+              rowsPerPage: 5, //设置每页显示的行数
+              source: _postsDataSource,
+              sortColumnIndex: _sortColumnIndex,
+              sortAscending: _sortAscending,
+              columns: [
+                DataColumn(
+                  label: Text('Title'),
+                  onSort: (int columnIndex, bool ascending) {
+                    _postsDataSource._sort(
+                        (post) => post.title.length, ascending);
+                    setState(() {
+                      _sortColumnIndex = columnIndex;
+                      _sortAscending = ascending;
+                    });
+                  },
+                  // 使用container设置宽度
+                  // label: Container(
+                  //   width: 150.0,
+                  //   child: Text('Title'),
+                  // ),
+                ),
+                DataColumn(
+                  label: Text('Author'),
+                ),
+                DataColumn(
+                  label: Text('Image'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
