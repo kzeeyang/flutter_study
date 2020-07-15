5cceb61303e0c53f6ff16d449aea35ba8f7292fa
kzeeyang <2402989406@qq.com>
2020-07-15 11:24:41 +0800

    stream demo

--- lib/demo/stream/stream_demo.dart
+++ lib/demo/stream/stream_demo.dart
@@ -20,17 +20,36 @@ class StreamDemoHome extends StatefulWidget {
 }
 
 class _StreamDemoHomeState extends State<StreamDemoHome> {
+  StreamSubscription _streamDemoSubscription;
+  StreamController<String> _streamDemo;
+  StreamSink _sinkDemo;
+  String _data = '...';
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _streamDemo.close(); //小部件移除时执行
+    super.dispose();
+  }
+
   @override
   void initState() {
     super.initState();
-    Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    // Stream<String> _streamDemo = Stream.fromFuture(fetchData());
+    print('Create a stream.');
+    _streamDemo = StreamController<String>();
+    _sinkDemo = _streamDemo.sink;
 
     print('Start listening on a stream.');
-    _streamDemo.listen(onData, onError: onError, onDone: onDone);
+    _streamDemoSubscription =
+        _streamDemo.stream.listen(onData, onError: onError, onDone: onDone);
     print('Initialized completed.');
   }
 
   void onData(String data) {
+    setState(() {
+      _data = data;
+    });
     print(data);
   }
 
@@ -43,17 +62,77 @@ class _StreamDemoHomeState extends State<StreamDemoHome> {
   }
 
   Future<String> fetchData() async {
-    await Future.delayed(Duration(seconds: 3));
-    throw 'Something happend';
-    // return 'hello~';
+    await Future.delayed(Duration(seconds: 2));
+    // throw 'Something happend';
+    return 'hello~';
+  }
+
+  void _addDataToStream() async {
+    print('Add data to stream');
+    String data = await fetchData();
+    // _streamDemo.add(data);
+    _sinkDemo.add(data);
+  }
+
+  void _pauseStream() {
+    print('Pause subscription');
+    _streamDemoSubscription.pause();
+  }
+
+  void _resumeStream() {
+    print('Resum subscription');
+    _streamDemoSubscription.resume();
+  }
+
+  void _cancelStream() {
+    print('Cancel subscription');
+    _streamDemoSubscription.cancel();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('StreamDemoHome'),
+        title: Text('StreamDemo'),
         elevation: 0.0,
+      ),
+      body: Container(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(_data),
+              // StreamBuilder(
+              //   stream: _streamDemo.stream,
+              //   initialData: '...',
+              //   builder: (context, snapshot) {
+              //     return Text('${snapshot.data}');
+              //   },
+              // ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  FlatButton(
+                    child: Text('Add'),
+                    onPressed: _addDataToStream,
+                  ),
+                  FlatButton(
+                    child: Text('Pause'),
+                    onPressed: _pauseStream,
+                  ),
+                  FlatButton(
+                    child: Text('Resum'),
+                    onPressed: _resumeStream,
+                  ),
+                  FlatButton(
+                    child: Text('Cancel'),
+                    onPressed: _cancelStream,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
